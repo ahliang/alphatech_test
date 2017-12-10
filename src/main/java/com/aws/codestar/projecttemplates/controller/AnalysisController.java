f89e9caadfc6704c1c97737d7706f00c33d4190e
Daren Tan <darentan94@gmail.com>
2017-12-10 15:49:42 +0800

    Updated Analysis Controller
    Non-functioning run Python

--- src/main/java/com/aws/codestar/projecttemplates/controller/AnalysisController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/AnalysisController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import java.io.IOException;
+import java.util.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.*; 
+import org.springframework.util.*;
+
 
 /**
  * Basic Spring MVC controller that handles all GET requests.
  */
 @Controller
-@RequestMapping("/analysis")
 public class AnalysisController {
 
     private final String siteName;
@@ -17,12 +24,23 @@ public class AnalysisController {
     public AnalysisController() {
         this.siteName = "analysis";
     }
-    
-    @RequestMapping(method = RequestMethod.GET)
+
+    @RequestMapping(value = "/analysis", method = RequestMethod.GET)
     public ModelAndView index() {
         ModelAndView mav = new ModelAndView("analysis");
         mav.addObject("siteName", this.siteName);
         return mav;
+    }
+
+    @RequestMapping(value = "/perform-analysis", method = RequestMethod.POST)
+    public String runAnalysisScript(HttpServletRequest request) throws IOException {
+        
+        String url_link = request.getParameter("url_link");
+        String anal_type = request.getParameter("anal_type");
+
+        Process p = Runtime.getRuntime().exec("python C:\\script\\hello.py", arg=[url_link, anal_type]);
+
+        return "Python script successfully run!";                                                
     }
 
 }
